7af17c44a4636a07b54b81a898df10d36d0ce0db
nguyenpl117 <nguyenpl117@gmail.com>
2020-05-25 15:05:15 +0700

    chore: update index base model

--- src/Orm/BaseModel/index.ts
+++ src/Orm/BaseModel/index.ts
@@ -4,4 +4,8 @@
  * Date: 5/19/2020
  * Time: 5:20 PM
  */
-export * from './BaseModel';
+import {LucidModel} from "../../Contracts/Model/LucidModel";
+
+const { BaseModel }: { BaseModel: LucidModel } = require('./BaseModel');
+
+export { BaseModel }
